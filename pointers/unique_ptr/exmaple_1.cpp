486787f46374a5d669344e6487ea0c7385cb44b1
s.stepniewski <s.stepniewski@elpromaelectronics.com>
2022-06-08 09:27:57 +0200

    Added first example.

---
+++ pointers/unique_ptr/exmaple_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <memory>
+
+
+class Resource{
+
+public:
+    Resource( ) { std::cout << "Resource acquired" << std::endl;}
+    ~Resource( ) { std::cout << "Resource destroyed" << std::endl;}
+};
+
+int main(){
+    std::unique_ptr<Resource> res{ new Resource()};
+
+    return 0;
+}
